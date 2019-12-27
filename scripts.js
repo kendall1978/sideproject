fd5bdcf4d15c2a1a3f42540a739cb0ccfcab161c
kendall1978 <46752584+kendall1978@users.noreply.github.com>
2019-12-26 21:46:01 -0600

    update

--- scripts.js
+++ scripts.js
@@ -3,66 +3,55 @@ Kendall Roberts
 Video Game Database
 */
 
-var mygames = [];
+var myGames = [];
 var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://rawg-video-games-database.p.rapidapi.com/games",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-		"x-rapidapi-key": "d7727d5987msh950787fe21acfc7p1ed9d9jsn957ca8def9ae"
-	}
-}
-$(document).ready(() =>{
-    $.ajax(settings).done(function (response) {
-        mygames.push(response.results)
-        console.log(mygames)
-    });
- })
-
- console.log(mygames);
-
-//  Vue.component('games-list',{
-//      props:['thegames'],
-//      template:
-//      `
-//      <div class="container">
-//      <div class="row">
-//          <h2>Carousel Example</h2>
-//          <div id="myCarousel" class="carousel slide" data-ride="carousel">
-
-//              <!-- Wrapper for slides -->
-//              <div class="carousel-inner" v-for="game in thegames>
-//                     <div class=" item active">
-//                  <img v-bind:src="game[0].background_image" alt="" style="width:100%;">
-//              </div>
-//          </div>
-
-//          <!-- Left and right controls -->
-//          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-//              <span class="glyphicon glyphicon-chevron-left"></span>
-//              <span class="sr-only">Previous</span>
-//          </a>
-//          <a class="right carousel-control" href="#myCarousel" data-slide="next">
-//              <span class="glyphicon glyphicon-chevron-right"></span>
-//              <span class="sr-only">Next</span>
-//          </a>
-//      </div>
-//  </div>
-//      `
-//  })
-
-
-var app = new Vue({
-    el:'#app',
-    data:{
-        games: mygames
-
+    "async": true,
+    "crossDomain": true,
+    "url": "https://rawg-video-games-database.p.rapidapi.com/games",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+        "x-rapidapi-key": "d7727d5987msh950787fe21acfc7p1ed9d9jsn957ca8def9ae"
     }
+}
+$(document).ready(() => {
+    $.ajax(settings).done(function (response) {
+        console.log(response.results)
+        myGames.push(response.results[0], response.results[1], response.results[2],
+            response.results[3], response.results[4], response.results[5], response.results[6],
+            response.results[7], response.results[8], response.results[9], response.results[10],
+            response.results[11], response.results[12], response.results[13], response.results[14],
+            response.results[15], response.results[16], response.results[17], response.results[18],
+            response.results[19], )
+        console.log(myGames)
+    });
 })
 
 
+Vue.component('games-list', {
+    props: ['thegames'],
+    template: `
+    <div class="container-fluid">
+        <div class="row">
+            <div v-for="item in thegames">
+                <div class="card" style="width: 18rem;">
+                <img class="card-img-top" v-bind:src="item.background_image" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{item.name}}</h5>
+                    <p class="card-text">{{item.rating}}</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+     `
+})
 
 
+var app = new Vue({
+    el: '#app',
+    data: {
+        games: myGames
 
+    }
+})
